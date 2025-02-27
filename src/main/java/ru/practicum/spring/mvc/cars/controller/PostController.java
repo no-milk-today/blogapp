03576c0c3c4b4b566556a82b22eabbaefe4a3e7f
no-milk-today <drmhere@yandex.by>
2025-02-27 18:31:51 +0300

    #add/delete comment via AJAX (web part)

--- src/main/java/ru/practicum/spring/mvc/cars/controller/PostController.java
+++ src/main/java/ru/practicum/spring/mvc/cars/controller/PostController.java
@@ -6,10 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.spring.mvc.cars.dto.CommentDto;
 import ru.practicum.spring.mvc.cars.dto.PostDto;
+import ru.practicum.spring.mvc.cars.service.CommentService;
 import ru.practicum.spring.mvc.cars.service.PostService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -17,15 +20,19 @@ import java.util.Map;
 public class PostController {
 
     private final PostService postService;
+    private final CommentService commentService;
 
-    public PostController(final PostService postService) {
+    public PostController(PostService postService, CommentService commentService) {
         this.postService = postService;
+        this.commentService = commentService;
     }
 
     @GetMapping("/details")
     public String showPostDetails(@RequestParam("postId") Long theId, Model theModel) {
         PostDto post = postService.findById(theId);
+        List<CommentDto> comments = commentService.findAllByPostId(theId);
         theModel.addAttribute("post", post);
+        theModel.addAttribute("comments", comments);
         return "posts/post-details";
     }
 
@@ -67,7 +74,9 @@ public class PostController {
 
     @GetMapping("/delete")
     public String delete(@RequestParam("postId") Long theId) {
+        commentService.deleteCommentsByPostId(theId);
         postService.deletePost(theId);
+
         return "redirect:/posts/list";
     }
 
@@ -90,5 +99,18 @@ public class PostController {
         response.put("likeCount", updatedPost.getLikeCount());
 
         return response;
+    }
+
+    @PostMapping("/addComment")
+    @ResponseBody
+    public CommentDto addComment(@RequestBody CommentDto commentDto) {
+        commentService.addComment(commentDto);
+        return commentDto;
+    }
+
+    @GetMapping("/deleteComment")
+    public String deleteComment(@RequestParam("commentId") Long commentId, @RequestParam("postId") Long postId) {
+        commentService.deleteComment(commentId);
+        return "redirect:/posts/details?postId=" + postId;
     }
 }
