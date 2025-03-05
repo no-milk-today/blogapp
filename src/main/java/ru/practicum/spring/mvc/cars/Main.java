57975b6d410693b9e4363568a56a13c88c30d09d
no-milk-today <drmhere@yandex.by>
2025-03-05 18:44:25 +0300

    #make use of environment variable (for pg user)

--- src/main/java/ru/practicum/spring/mvc/cars/Main.java
+++ src/main/java/ru/practicum/spring/mvc/cars/Main.java
@@ -1,16 +1,21 @@
 package ru.practicum.spring.mvc.cars;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
+        var applicationContext = SpringApplication.run(Main.class, args);
+        var pgUsername = System.getenv("PG_USERNAME");
+        LOG.info("PG_USERNAME: {}", pgUsername);
+//        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            System.out.println(beanDefinitionName);
+//        }
     }
 }
