f1ba8be9901417bfddaeff6eaf5ed9a6a2ec7924
no-milk-today <drmhere@yandex.by>
2025-03-05 17:32:12 +0300

    #fix lombok issue, change main class

---
+++ src/main/java/ru/practicum/spring/mvc/cars/Main.java
@@ -0,0 +1,16 @@
+package ru.practicum.spring.mvc.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+    }
+}
